dca575afa6dbd1f7b3845063927e99fd4868ab37
Cytosol / Nicholas <lolrandomroblox@gmail.com>
2017-04-12 11:55:44 -0400

    updated

--- resources/files.lua
+++ resources/files.lua
@@ -1,1 +1,9 @@
-
+-- Shine Resources Directory files.lua
+file_list = {
+  pictures = "Pictures",
+  downloads = "Downloads",
+  documents = "Documents"
+}
+local file
+home_dir = "/home"
+cmd_cat = os.execute("cat "..file)
